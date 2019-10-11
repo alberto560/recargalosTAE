dc05053ba7e8f9875cf20210920c3908603581e9
alberto560 <albertodelacruz560@gmail.com>
2019-10-10 21:31:01 -0500

    Add Ajax_Web

---
+++ controller/Ajax_Web.php
@@ -0,0 +1,38 @@
+<?php
+	include_once("../model/ConsultaProcesados.php");
+/**
+ *
+ */
+	class ajax_web
+	{
+	private $model;
+		function __construct()
+		{
+				$this->model = new ConsultaProcesados();
+		}
+		public function consultar(){
+		 $consulta= $this->model->get_Recargas();
+	   	 echo $consulta;
+		}
+
+		public function consultar_all(){
+		 $consulta= $this->model->get_All();
+	   	 echo $consulta;
+		}
+	}
+	$consulta_Store= new ajax_web();
+
+	if(!isset($_POST['P'])) {
+		print json_encode(0);
+		return;
+	}
+
+	switch($_POST['P']) {
+		case 'OPERADORES_APP':			
+      		$consulta_Store->consultar();
+		break;	
+		case 'RECARGAS-PR_APP':			
+      		$consulta_Store->consultar_all();
+		break;	
+	}
+?>
---
+++ model/ConsultaProcesados.php
@@ -0,0 +1,19 @@
+
+<?php
+
+	class ConsultaProcesados {
+			function get_Recargas()
+			{
+				//$sentencia = array('user' => array( array('firstName' => 'Vignesh'), array('firstName' => 'Vaibhav') ));		
+				$sentencia = array('Operadores' => array( array('TMAC PV'), array('CARLOS' )));
+				//$sentencia2 =array('Msportada'=> array('MsSistema' => array('T'=> 'INSTALA/ACTUALIZA TU DESKTOP APP' ) ));
+				return json_encode($sentencia);
+			/*	{"Operadores":[["TMAC PV"],["CARLOS"]],"Msportada":{"MsSistema":{"T":"INSTALA\/ACTUALIZA TU DESKTOP APP","L":["Actualiza Vigencias de Paquetes SinLimite","Y Vigencias Paquetes de Internet","Nueva Recarga Entretenimiento","Descarga la version 4.9<\/a>"]},"VCTtmac":"1"}}*/
+			}	
+			function get_All(){
+				$sentencia = array('TB_Recargas' => array( array('0','NO SE ENCONTRARON REGISTROS EN ESTA FECHA' ),array('T','0')));
+				return json_encode($sentencia);
+			}		
+
+	}
+?>
